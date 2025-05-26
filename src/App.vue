2e00556fb516e2407d39ae02b77a176c165b3782
AndriiPyrohovskyi <cbkgfst@gmail.com>
2025-05-26 21:54:33 +0300

    set up Vue 3 project with Vite, Tailwind CSS, and TypeScript

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="bg-blue-500 text-white p-4">
+    <h1 class="text-2xl font-bold">Hello Tailwind!</h1>
+  </div>
+
+  <RouterView />
+</template>
+
+<style scoped>
+</style>
